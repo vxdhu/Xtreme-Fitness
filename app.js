805f5cd75c5c5450e21f73e255fcc24e7036b0ae
Vidhu Shekhar <twrvidhu18@gmail.com>
2025-04-23 20:26:11 +0530

    updates

--- app.js
+++ app.js
@@ -1,27 +1,31 @@
-$( '.js-input' ).keyup(function() {
-    if( $(this).val() ) {
-       $(this).addClass('not-empty');
-    } else {
-       $(this).removeClass('not-empty');
-    }
-  });
+// $( '.js-input' ).keyup(function() {
+//     if( $(this).val() ) {
+//        $(this).addClass('not-empty');
+//     } else {
+//        $(this).removeClass('not-empty');
+//     }
+//   });
 
 
-  $(document).ready(function(){
-    $(".testimonial .indicators li").click(function(){
-      var i = $(this).index();
-      var targetElement = $(".testimonial .tabs li");
-      targetElement.eq(i).addClass('active');
-      targetElement.not(targetElement[i]).removeClass('active');
-              });
-              $(".testimonial .tabs li").click(function(){
-                  var targetElement = $(".testimonial .tabs li");
-                  targetElement.addClass('active');
-                  targetElement.not($(this)).removeClass('active');
-              });
-          });
-  $(document).ready(function(){
-      $(".slider .swiper-pagination span").each(function(i){
-          $(this).text(i+1).prepend("0");
-      });
-  });
+//   $(document).ready(function(){
+//     $(".testimonial .indicators li").click(function(){
+//       var i = $(this).index();
+//       var targetElement = $(".testimonial .tabs li");
+//       targetElement.eq(i).addClass('active');
+//       targetElement.not(targetElement[i]).removeClass('active');
+//               });
+//               $(".testimonial .tabs li").click(function(){
+//                   var targetElement = $(".testimonial .tabs li");
+//                   targetElement.addClass('active');
+//                   targetElement.not($(this)).removeClass('active');
+//               });
+//           });
+//   $(document).ready(function(){
+//       $(".slider .swiper-pagination span").each(function(i){
+//           $(this).text(i+1).prepend("0");
+//       });
+//   });
+
+
+
+
