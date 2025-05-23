a7cc645f48cccd1315ca3121154b8547b6617c5b
vxdhu <twrvidhu18@gmail.com>
2025-05-23 21:14:56 +0530

    updates

--- app.js
+++ app.js
@@ -1,31 +1,5 @@
-// $( '.js-input' ).keyup(function() {
-//     if( $(this).val() ) {
-//        $(this).addClass('not-empty');
-//     } else {
-//        $(this).removeClass('not-empty');
-//     }
-//   });
-
-
-//   $(document).ready(function(){
-//     $(".testimonial .indicators li").click(function(){
-//       var i = $(this).index();
-//       var targetElement = $(".testimonial .tabs li");
-//       targetElement.eq(i).addClass('active');
-//       targetElement.not(targetElement[i]).removeClass('active');
-//               });
-//               $(".testimonial .tabs li").click(function(){
-//                   var targetElement = $(".testimonial .tabs li");
-//                   targetElement.addClass('active');
-//                   targetElement.not($(this)).removeClass('active');
-//               });
-//           });
-//   $(document).ready(function(){
-//       $(".slider .swiper-pagination span").each(function(i){
-//           $(this).text(i+1).prepend("0");
-//       });
-//   });
-
-
+const blogs = [
+    { blogId: 1, title: "Blog 1", content: "Content of Blog 1" },
+]
 
 
